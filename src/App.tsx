526fdcbdd3ecd09299bd0b672fd5cdc17b632b02
LilHeizy <vishnyakov98@gmail.com>
2025-04-03 00:04:31 +0200

    named import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Auth from "remoteApp/Auth";
-import useStore from "remoteApp/Store";
+import { useInput } from "remoteApp/Store";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useStore();
+  const [value, setValue] = useInput();
   return (
     <div className="App">
       <h1>Host Application</h1>
